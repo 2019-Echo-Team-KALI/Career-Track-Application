a61a2a071ed64d9e67d9e47e3d1ce1317af15e4f
Lisa Kamino <lisakamino@MacBook-Air-2.local>
2019-12-06 11:34:06 -0800

    about us updated with logos with links to github and linked in profiles

--- app/javascript/components/HomePage.js
+++ app/javascript/components/HomePage.js
@@ -4,6 +4,8 @@ import lisa from './lisakamino.jpg'
 import kalvin from './kalvinkang.jpg'
 import ian from './ianthompson.jpg'
 import arvin from './arvinlleva.jpg'
+import github from './github.png'
+import linkedin from './linkedin.png'
 
 function HomePage() {
     return (
@@ -16,26 +18,42 @@ function HomePage() {
           <h2>About Us</h2>
             <div className = "firstbios">
               <div className="bio">
-                <img src={arvin} />
+                <img src={arvin} className="biophoto"/>
                 <p>Arvin Lleva<br />
                 Full-Stack Web Developer</p>
+                <div className="logo">
+                  <a href = "https://github.com/adclleva"><img src={github} /></a>
+                  <a href = "https://www.linkedin.com/in/arvinlleva/"><img src={linkedin} /></a>
+                </div>
               </div>
               <div className="bio">
-                <img src={ian} />
+                <img src={ian} className="biophoto"/>
                 <p>Ian Thompson<br />
                 Full-Stack Web Developer</p>
+                <div className="logo">
+                  <a href="https://github.com/thompsian"><img src={github} /></a>
+                  <a href= "https://www.linkedin.com/in/thompsian/"><img src={linkedin} /></a>
+                </div>
               </div>
             </div>
             <div className="secondbios">
               <div className="bio">
-                <img src={kalvin} />
+                <img src={kalvin} className="biophoto"/>
                 <p>Kalvin Kang<br />
                 Full-Stack Web Developer</p>
+                <div className="logo">
+                  <a href="https://github.com/kalvin-k"><img src={github} /></a>
+                  <a href="https://www.linkedin.com/in/kalvin-k/"><img src={linkedin} /></a>
+                </div>
               </div>
               <div className="bio">
-                <img src={lisa} />
+                <img src={lisa} className="biophoto"/>
                 <p>Lisa Kamino<br />
                 Full-Stack Web Developer</p>
+                <div className="logo">
+                  <a href="https://github.com/lk15"><img src={github} /></a>
+                  <a href="https://www.linkedin.com/in/lisakamino/"><img src={linkedin} /></a>
+                </div>
               </div>
             </div>
         </div>
