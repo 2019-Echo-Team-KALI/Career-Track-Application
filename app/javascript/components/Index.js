0edcc9182785be7202fccd043a5703e3662edb59
ArvinLleva <arvin.lleva@yahoo.com>
2019-12-10 13:00:15 -0800

    File Restructuring recommit

---
+++ app/javascript/components/Index.js
@@ -0,0 +1,30 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import App from "./App"
+import { BrowserRouter as Router } from "react-router-dom"
+import { ContextProvider } from "./context/Context"
+
+function Index(props) {
+
+    const {
+      logged_in,
+      sign_in_route,
+      sign_out_route,
+      current_user_id
+    } = props
+
+    return (
+        <ContextProvider>  
+            <Router>
+                <App logged_in={logged_in} sign_in_route={sign_in_route} sign_out_route={sign_out_route} current_user_id={current_user_id}/>
+            </Router>
+        </ContextProvider>  
+    );
+
+}
+
+export default Index
+
+
+
