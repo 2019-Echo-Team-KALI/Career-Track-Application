8c7ed80dd1e25af69d736444f3b18ed7f5a59a6c
Lisa Kamino <lisakamino@MacBook-Air-2.local>
2019-12-11 15:33:09 -0800

    reduced hero image size and added aboutapp section

---
+++ app/javascript/react/components/AboutApp.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+function AboutApp() {
+    return (
+      <React.Fragment>
+        <div style={{width: '80%', margin: '0 auto'}}>
+          <h3 style={{marginTop: '80px'}}>CareerTrack is a job hunt management software that enables job-seekers to keep track of jobs and associated tasks as they navigate their job search.</h3>
+        </div>
+      </React.Fragment>
+    );
+}
+
+export default AboutApp
--- app/javascript/react/pages/HomePage.js
+++ app/javascript/react/pages/HomePage.js
@@ -2,13 +2,14 @@ import React from "react"
 import PropTypes from "prop-types"
 import HomeCarousel from '../components/HomeCarousel.js'
 import AboutUs from '../components/AboutUs.js'
-
+import AboutApp from '../components/AboutApp.js'
 
 function HomePage() {
     return (
       <React.Fragment>
 
         <HomeCarousel />
+        <AboutApp />
         <AboutUs />
       </React.Fragment>
     );
