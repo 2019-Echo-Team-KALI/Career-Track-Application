98390145c9c42f5b234ec498211b80ffea4e7c92
ArvinLleva <arvin.lleva@yahoo.com>
2019-12-15 13:53:37 -0800

    Able to show the company name in the main task list through the the maintaskcomponent, the component was needed to load the specific job that we needed

--- app/javascript/react/components/MainTaskComponent.js
+++ app/javascript/react/components/MainTaskComponent.js
@@ -8,17 +8,49 @@ import { useState, useEffect } from 'react'
 
 function MainTaskComponent(props) { // this is being called from the App.js
 
-    const { title, description, job_id, user_id} = props // we need the job id to compare
+    const { title, description, job_id, user_id, apiJobsData} = props // we need the job id to compare
+    const [jobOfTask, setJobOfTask] = useState({})
 
-        return(
-            <div style={{textAlign: 'center', marginBottom: '0.5em'}}>
-                <Link to={`/jobs/${job_id}`}>
-                    <button className="btn btn-outline-warning" style={{borderRight: 'none',borderLeft: 'none',borderBottom: 'none',  width: '87%', color: 'black', textAlign: 'left'}}type="button" > 
-                        COMPANY <br /><div style={{textTransform: 'lowercase', opacity: 0.6}}>{title}</div> 
-                    </button>
-                </Link>
-            </div>
-        )
+    console.log(apiJobsData, "jobsdata", "---", )
+
+    useEffect(() => {
+        loadJob()
+    },[])
+ 
+
+    function getJob() {
+        return fetch(`/jobs/${job_id}`)
+            .then( resp => {
+                if (resp.status === 200) {
+                    return resp.json()
+                } else {
+                    return Promise.new(() => {   {/*need to fix this*/}
+                        resolve({error: 'there was an error'})
+                    })
+                }
+            })
+    }
+
+    function loadJob(){
+        getJob()
+            .then(job => {
+                if(job.errors) {
+                    setErrors(job.errors)
+                }
+                setJobOfTask(job)
+            })
+    }
+    
+
+    return(
+        <div style={{textAlign: 'center', marginBottom: '0.5em'}}>
+            <Link to={`/jobs/${job_id}`}>
+                <button className="btn btn-outline-warning" style={{borderRight: 'none',borderLeft: 'none',borderBottom: 'none',  width: '87%', color: 'black', textAlign: 'left'}}type="button" > 
+                    {jobOfTask.name} <br /><div style={{textTransform: 'lowercase', opacity: 0.6}}>{title}</div> 
+                </button>
+            </Link>
+        </div>
+    )
 }
 
 export default MainTaskComponent
